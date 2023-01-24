43e766553178107e85e81aaa19e1514e8647642a
Karen Del Rosario <karen.delrosario@macquarie.com>
2023-01-24 12:09:31 +1100

    Add test component

---
+++ test/test.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { TestComponent } from './test.component';
+
+@NgModule({
+    declarations: [
+        TestComponent
+    ],
+    imports: [
+        BrowserModule
+    ],
+    exports: [
+        TestComponent
+    ],
+    providers: [],
+})
+export class SharedModule { }
